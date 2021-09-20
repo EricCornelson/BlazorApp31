691b35d2346cda6f0dccdd0c562ecfd64be20a78
Eric Cornelson <ercornel@microsoft.com>
2021-09-20 10:22:57 -0700

    initial commit

---
+++ BlazorApp31/Client/Program.cs
@@ -0,0 +1,15 @@
+using BlazorApp31.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddSingleton(new ClassLibrary1.MyClass());
+
+await builder.Build().RunAsync();
+
---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassLibrary1
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+            Console.WriteLine("to construindo\n");
+        }
+        public int getCount()
+        {
+            return 10;
+        }
+    }
+}
